8b1572c425085b9667c04ac829b088ff219aa632
Cesar Garcia <CGarcia@heads-up.com>
2023-07-12 16:37:34 -0500

    Add Contact component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,45 @@
 import React from "react";
 
 export default function Contact() {
-    return (
-        <div>
-            <h1>Contact</h1>
+  return (
+    <div className="card m-3 p-3">
+        <h3 className="card-title">Contact</h3>
+      <form>
+      <div class="form-group">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder="Enter full name"
+          />
         </div>
-    )
+        <div class="form-group py-3">
+          <label for="email">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted">
+            I'll never share your email with anyone else.
+          </small>
+        </div>
+        <div class="form-group">
+          <label for="comments">Comments</label>
+          <textarea
+            type="text"
+            class="form-control"
+            id="name"
+            placeholder=""
+          />
+        </div>
+        <button type="submit" class="btn btn-primary my-3">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,7 +3,71 @@ import React from 'react';
 export default function Portfolio() {
     return (
         <div>
-            <h1>Portfolio</h1>
+            <h3 className="card-title m-3">Portfolio</h3>
+            <div className="row">
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 1</h5>
+                            <p className="card-text">Description of Project 1</p>
+                            <a href="" className="btn btn-primary">Go to Project 1</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 2</h5>
+                            <p className="card-text">Description of Project 2</p>
+                            <a href="" className="btn btn-primary">Go to Project 2</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 3</h5>
+                            <p className="card-text">Description of Project 3</p>
+                            <a href="" className="btn btn-primary">Go to Project 3</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 1</h5>
+                            <p className="card-text">Description of Project 1</p>
+                            <a href="" className="btn btn-primary">Go to Project 1</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 2</h5>
+                            <p className="card-text">Description of Project 2</p>
+                            <a href="" className="btn btn-primary">Go to Project 2</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card m-3">
+                        <img src="https://via.placeholder.com/150" className="card-img-top" alt="placeholder"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Project 3</h5>
+                            <p className="card-text">Description of Project 3</p>
+                            <a href="" className="btn btn-primary">Go to Project 3</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
