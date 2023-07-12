691c41d2160ca9959180eda1f6f3318a33b0e921
Cesar Garcia <CGarcia@heads-up.com>
2023-07-12 15:57:25 -0500

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,18 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <h2>Footer</h2>
+            <a 
+            href="linkedin.com"
+            onClick={() => window.open("https://www.linkedin.com/in/cesaralonsogarcia/")}
+            >
+            <img alt="LinkedIn" src="https://img.icons8.com/fluent/48/000000/linkedin.png"/>
+            </a>
+            <a
+            href="github.com"
+            onClick={() => window.open("https://www.github.com/cesaralonsogarcia/")}
+            >
+            <img alt="GitHub" src="https://img.icons8.com/fluent/48/000000/github.png"/>
+            </a>
         </footer>
     );
 }
